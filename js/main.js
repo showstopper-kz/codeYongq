ff467f48b26ec21857b3177f83b82dae3dd40810
zhuyongqi <abnormalled@outlook.com>
2021-04-09 16:31:15 +0800

    Site updated: 2021-04-09 16:31:15

--- js/main.js
+++ js/main.js
@@ -1,113 +1,140 @@
-/**
- * Sets up Justified Gallery.
- */
-if (!!$.prototype.justifiedGallery) {
-  var options = {
-    rowHeight: 140,
-    margins: 4,
-    lastRow: "justify"
-  };
-  $(".article-gallery").justifiedGallery(options);
+/* eslint-disable no-undef */
+// console.js
+console.log(String.raw`
+          _____                    _____                    _____                    _____          
+         /\    \                  /\    \                  /\    \                  /\    \         
+        /::\____\                /::\    \                /::\    \                /::\    \        
+       /:::/    /               /::::\    \               \:::\    \              /::::\    \       
+      /:::/    /               /::::::\    \               \:::\    \            /::::::\    \      
+     /:::/    /               /:::/\:::\    \               \:::\    \          /:::/\:::\    \     
+    /:::/____/               /:::/__\:::\    \               \:::\    \        /:::/__\:::\    \    
+   /::::\    \              /::::\   \:::\    \               \:::\    \      /::::\   \:::\    \   
+  /::::::\____\________    /::::::\   \:::\    \               \:::\    \    /::::::\   \:::\    \  
+ /:::/\:::::::::::\    \  /:::/\:::\   \:::\    \               \:::\    \  /:::/\:::\   \:::\    \ 
+/:::/  |:::::::::::\____\/:::/  \:::\   \:::\____\_______________\:::\____\/:::/__\:::\   \:::\____\
+\::/   |::|~~~|~~~~~     \::/    \:::\  /:::/    /\::::::::::::::::::/    /\:::\   \:::\   \::/    /
+ \/____|::|   |           \/____/ \:::\/:::/    /  \::::::::::::::::/____/  \:::\   \:::\   \/____/ 
+       |::|   |                    \::::::/    /    \:::\~~~~\~~~~~~         \:::\   \:::\    \     
+       |::|   |                     \::::/    /      \:::\    \               \:::\   \:::\____\    
+       |::|   |                     /:::/    /        \:::\    \               \:::\   \::/    /    
+       |::|   |                    /:::/    /          \:::\    \               \:::\   \/____/     
+       |::|   |                   /:::/    /            \:::\    \               \:::\    \         
+       \::|   |                  /:::/    /              \:::\____\               \:::\____\        
+        \:|   |                  \::/    /                \::/    /                \::/    /        
+         \|___|                   \/____/                  \/____/                  \/____/         
+see theme at https://github.com/0x4qE/hexo-theme-Kaze
+`);
+// darkmode.js
+// reverse button
+const scrollWidth = document.body.scrollWidth || document.documentElement.scrollWidth;
+let darkControlButton = null;
+if (scrollWidth <= 742) {
+  darkControlButton = document.querySelector('.darkwidget');
+} else {
+  darkControlButton = document.querySelector('.darknavbar');
 }
 
-$(document).ready(function() {
-
-  /**
-   * Shows the responsive navigation menu on mobile.
-   */
-  $("#header > #nav > ul > .icon").click(function() {
-    $("#header > #nav > ul").toggleClass("responsive");
-  });
-
-
-  /**
-   * Controls the different versions of  the menu in blog post articles 
-   * for Desktop, tablet and mobile.
-   */
-  if ($(".post").length) {
-    var menu = $("#menu");
-    var nav = $("#menu > #nav");
-    var menuIcon = $("#menu-icon, #menu-icon-tablet");
-
-    /**
-     * Display the menu on hi-res laptops and desktops.
-     */
-    if ($(document).width() >= 1440) {
-      menu.css("visibility", "visible");
-      menuIcon.addClass("active");
-    }
-
-    /**
-     * Display the menu if the menu icon is clicked.
-     */
-    menuIcon.click(function() {
-      if (menu.css("visibility") === "hidden") {
-        menu.css("visibility", "visible");
-        menuIcon.addClass("active");
-      } else {
-        menu.css("visibility", "hidden");
-        menuIcon.removeClass("active");
-      }
-      return false;
-    });
-
-    /**
-     * Add a scroll listener to the menu to hide/show the navigation links.
-     */
-    if (menu.length) {
-      $(window).on("scroll", function() {
-        var topDistance = menu.offset().top;
-
-        // hide only the navigation links on desktop
-        if (!nav.is(":visible") && topDistance < 50) {
-          nav.show();
-        } else if (nav.is(":visible") && topDistance > 100) {
-          nav.hide();
-        }
-
-        // on tablet, hide the navigation icon as well and show a "scroll to top
-        // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
-          $("#menu-icon-tablet").show();
-          $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
-          $("#menu-icon-tablet").hide();
-          $("#top-icon-tablet").show();
-        }
-      });
-    }
-
-    /**
-     * Show mobile navigation menu after scrolling upwards,
-     * hide it again after scrolling downwards.
-     */
-    if ($( "#footer-post").length) {
-      var lastScrollTop = 0;
-      $(window).on("scroll", function() {
-        var topDistance = $(window).scrollTop();
-
-        if (topDistance > lastScrollTop){
-          // downscroll -> show menu
-          $("#footer-post").hide();
-        } else {
-          // upscroll -> hide menu
-          $("#footer-post").show();
-        }
-        lastScrollTop = topDistance;
-
-        // close all submenu"s on scroll
-        $("#nav-footer").hide();
-        $("#toc-footer").hide();
-        $("#share-footer").hide();
-
-        // show a "navigation" icon when close to the top of the page, 
-        // otherwise show a "scroll to the top" icon
-        if (topDistance < 50) {
-          $("#actions-footer > #top").hide();
-        } else if (topDistance > 100) {
-          $("#actions-footer > #top").show();
-        }
-      });
+darkControlButton.addEventListener('click', () => {
+  setDarkmode(reverseDarkModeSetting());
+});
+// scroll-up.js
+const smoothScrollToTop = () => {
+  let yTopValve = (window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop);
+  if (yTopValve > 1) {
+    window.requestAnimationFrame(smoothScrollToTop);
+    scrollTo(0, Math.floor(yTopValve * 0.85));
+  } else {
+    scrollTo(0, 0);
+  }
+};
+setTimeout(() => {
+  document.getElementById('scrollbutton').onclick = smoothScrollToTop;
+}, 0);
+// popbutton.js
+const reversePopButton = () => {
+  const scrollButton = document.getElementById('scrollbutton');
+  const menuButton = document.getElementById('menubutton');
+  const reverseButton = document.getElementById('popbutton');
+  const scrollWidth = document.body.scrollWidth || document.documentElement.scrollWidth;
+  if (scrollButton.style.display === 'flex') {
+    scrollButton.style.bottom = '32px';
+    scrollButton.style.opacity = '0';
+    reverseButton.style.transform = 'none';
+    setTimeout(() => {
+      scrollButton.style.display = 'none';
+    }, 100);
+  } else {
+    scrollButton.style.display = 'flex';
+    reverseButton.style.transform = 'rotate(90deg)';
+    setTimeout(() => {
+      scrollButton.style.bottom = '85px';
+      scrollButton.style.opacity = '1';
+    }, 100);
+  }
+  const mobileToc = document.getElementById('mobiletoc');
+  if (scrollWidth <= 862 && mobileToc) {
+    if (menuButton.style.display === 'flex') {
+      menuButton.style.right = '32px';
+      menuButton.style.opacity = '0';
+      setTimeout(() => {
+        menuButton.style.display = 'none';
+      }, 100);
+    } else {
+      menuButton.style.display = 'flex';
+      setTimeout(() => {
+        menuButton.style.right = '85px';
+        menuButton.style.opacity = '1';
+      }, 100);
     }
   }
-});
+  const darkButton = document.querySelector('.darkwidget');
+  if (scrollWidth <= 742) {
+    if (darkButton.style.display === 'flex') {
+      darkButton.style.bottom = '32px';
+      darkButton.style.opacity = '0';
+      darkButton.style.transform = 'none';
+      setTimeout(() => {
+        darkButton.style.display = 'none';
+      }, 100);
+    } else {
+      darkButton.style.display = 'flex';
+      reverseButton.style.transform = 'rotate(90deg)';
+      setTimeout(() => {
+        darkButton.style.bottom = '138px';
+        darkButton.style.opacity = '1';
+      }, 100);
+    }
+  } 
+};
+setTimeout(() => {
+  document.getElementById('popbutton').onclick = reversePopButton;
+}, 0);
+// menuButton.js
+function menuClick(event) {
+  const target = event.target;
+  const mobileToc = document.getElementById('mobiletoc');
+  if (!mobileToc) {
+    return;
+  }
+  if (!mobileToc.contains(target)) {
+    mobileToc.style.display = 'none';
+    document.body.removeChild(mask);
+    document.removeEventListener('click', menuClick);
+  }
+}
+const clickMenuButton = () => {
+  const mobileToc = document.getElementById('mobiletoc');
+  if (!mobileToc) {
+    return;
+  }
+  mobileToc.style.display = 'block';
+  const mask = document.createElement('div');
+  mask.id = 'mask';
+  document.body.appendChild(mask);
+  setTimeout(() => {
+    document.addEventListener('click', menuClick);
+  }, 0);
+};
+setTimeout(() => {
+  document.getElementById('menubutton').onclick = clickMenuButton;
+}, 0);
